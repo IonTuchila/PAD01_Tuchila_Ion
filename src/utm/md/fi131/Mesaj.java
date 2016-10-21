695e58dff809fbe6af1fd571a5ac494a59f73a58
Boris <b.fedco@gmail.com>
2016-10-21 22:27:58 +0300

    first commit

---
+++ src/utm/md/fi131/Mesaj.java
@@ -0,0 +1,12 @@
+package utm.md.fi131;
+
+public class Mesaj {
+	public Mesaj(String command, String message) {
+		this.command=command;
+		this.message=message;
+	}
+	public Mesaj() {}
+	public String command;
+    public String message;
+    
+    }
